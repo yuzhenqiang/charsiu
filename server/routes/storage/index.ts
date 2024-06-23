3cb5d1cc13dd85206b6e1aa140f8e22cc0e9d276
zhenqiang Yu <zhenqiang.yu@foxmail.com>
2024-06-23 23:31:21 +0800

    update 抽离目录获取方法

--- server/routes/storage/index.ts
+++ server/routes/storage/index.ts
@@ -1,27 +1,32 @@
 import { resolve, join } from 'node:path'
 import { readdir, stat } from 'node:fs/promises'
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import mime from 'mime'
 
-const storagePath = resolve(process.env.STORAGE_PATH!)
-
 export const storage = new Hono()
+
+/** 解析请求的目录 */
+const getStoragePath = (ctx: Context) => ctx.req.path.slice(ctx.req.routePath.length - 2)
+/** 获取真实目录路径 */
+const getRealDir = (relativePath: string) => join(process.env.STORAGE_PATH!, relativePath)
+
+
 
 /** 获取目录下的文件信息 */
 storage.get('*', async (ctx) => {
   try {
-    /** 请求地址 */
-    const requestPath = ctx.req.path.slice(ctx.req.routePath.length - 2)
-    /** 存储目录 */
-    const storageDir = join(storagePath, requestPath)
+    /** 请求目录 */
+    const storagePath = getStoragePath(ctx)
+    /** 真实目录 */
+    const realDir = getRealDir(storagePath)
     /** 文件名称 */
-    const fileNames = await readdir(storageDir)
+    const fileNames = await readdir(realDir)
     /** 文件对象集合 */
     const files = await Promise.all(fileNames.map(async filename => {
-      const info = await stat(join(storageDir, filename))
+      const info = await stat(join(realDir, filename))
       const file: FileItem = {
         name: filename,
-        path: join(requestPath, filename),
+        path: join(storagePath, filename),
         size: info.size,
         createTime: info.ctimeMs,
         modifyTime: info.mtimeMs,
@@ -30,7 +35,7 @@ storage.get('*', async (ctx) => {
       }
       return file
     }))
-    return ctx.json({ path: requestPath, files: files })
+    return ctx.json({ path: storagePath, files: files })
   } catch (err) {
     console.log(err)
     if (err instanceof Error && 'code' in err) {
